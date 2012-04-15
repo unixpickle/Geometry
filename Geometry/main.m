150b7483cc8036a0c8e691e88f50b0e697fbfaa0
Alex Nichol <alexnichol@comcast.net>
2012-04-15 10:27:59 -0400

    Initial Commit

---
+++ Geometry/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Geometry
+//
+//  Created by Alex Nichol on 4/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ANAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ANAppDelegate class]));
+    }
+}
